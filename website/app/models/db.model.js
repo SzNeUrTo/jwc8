34fc1d8d9b27785221c03cf46751ad5ca6b4f0d3
Supanut <supanut.overload@gmail.com>
2016-03-13 04:32:35 +0700

    passportjs -> Facebook + Local

--- website/app/models/db.model.js
+++ website/app/models/db.model.js
@@ -1,10 +1,41 @@
+// var mongoose = require('mongoose');
+
+// var Schema = mongoose.Schema;
+
+// var userSchema = new Schema({
+// 	name: String
+// });
+
+// var User = mongoose.model('User', userSchema);
+// module.exports = User;
+
 var mongoose = require('mongoose');
-
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-	name: String
+var bcrypt = require('bcrypt');
+var userSchema = mongoose.Schema({
+	local: {
+		username: String,
+		password: String
+	},
+	facebook: {
+		id: String,
+		token: String,
+		email: String,
+		name: String
+	},
+	google: {
+		id: String,
+		token: String,
+		email: String,
+		name: String
+	}
 });
 
-var User = mongoose.model('User', userSchema);
-module.exports = User;
+userSchema.methods.generateHash = function(password){
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(9));
+}
+
+userSchema.methods.validPassword = function(password){
+	return bcrypt.compareSync(password, this.local.password);
+}
+
+module.exports = mongoose.model('User', userSchema);
