b140010090d215691ba9da896828765197d10872
Supanut <supanut.overload@gmail.com>
2016-03-14 14:52:34 +0700

    Update Schema (JWC)

--- website/app/models/db.model.js
+++ website/app/models/db.model.js
@@ -1,37 +1,69 @@
-// var mongoose = require('mongoose');
-
-// var Schema = mongoose.Schema;
-
-// var userSchema = new Schema({
-// 	name: String
-// });
-
-// var User = mongoose.model('User', userSchema);
-// module.exports = User;
-
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
-var userSchema = mongoose.Schema({
-	// auth_type: String,
-	auth: {
-		auth_type: String,
-		local: {
-			username: String,
-			password: String
-		},
-		facebook: {
-			id: String,
-			token: String,
-			email: String,
-			name: String
-		},
-		google: {
- 			id: String,
- 			token: String,
- 			email: String,
- 			name: String
-		}
+
+var authSchema = mongoose.Schema({
+	auth_type: String,
+	local: {
+		username: String,
+		password: String
+	},
+	facebook: {
+		id: String,
+		token: String,
+		email: String,
+		name: String
+	},
+	google: {
+		id: String,
+		token: String,
+		email: String,
+		name: String
 	}
+});
+
+var profileSchema = mongoose.Schema({
+	firstname: String,
+	lastname: String,
+	nickname: String,
+	birthdate: String, // or Date
+	sex: String,
+	age: Number,
+	religion: String,
+	school: String,
+	level: String,
+	major: String,
+	position: String,
+	tel: String,
+	email: String,
+	size: String,
+	contact_person: {
+		firstname: String,
+		lastname: String,
+		relation: String,
+		emergency_tel: String
+	}
+
+});
+
+var answerSchema = mongoose.Schema({
+	answer: String,
+	point: Number
+});
+
+var jwcinfoSchema = mongoose.Schema({
+	major: String,
+	generalquestion : {
+		answers : [answerSchema]
+	},
+	specialquestion : {
+		answers : [answerSchema]
+	}
+});
+
+var userSchema = mongoose.Schema({
+	auth: authSchema,
+	profile: profileSchema,
+	jwcinfo: jwcinfoSchema,
 });
 
 userSchema.methods.generateHash = function(password){
