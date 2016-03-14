afab6c2877757cbd98b9d4d9b9f063a38573460f
komcal <kom.sujautra@gmail.com>
2016-03-15 00:46:47 +0700

    set complete path

--- website/app/routes.app.js
+++ website/app/routes.app.js
@@ -2,4 +2,5 @@ module.exports = function(app) {
 	require('./fortest.app.js')(app);
 	require('../app/routes/index.routes')(app);
 	require('../app/routes/register.routes')(app);
+	require('../app/routes/complete.routes')(app);
 }
