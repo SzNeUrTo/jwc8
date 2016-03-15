33ac2e5dc48ade444c248e311b3da31df9439867
komcal <kom.sujautra@gmail.com>
2016-03-15 13:36:42 +0700

    add form

--- website/app/controllers/complete.controller.js
+++ website/app/controllers/complete.controller.js
@@ -1,3 +1,7 @@
 exports.render = function(req, res) {
 	res.render('test');
 }
+exports.renderPost = function(req, res){
+	console.log(req.body);
+	res.render('test');
+}
--- website/app/routes/complete.routes.js
+++ website/app/routes/complete.routes.js
@@ -1,4 +1,5 @@
 module.exports = function(app){
     var complete = require('../controllers/complete.controller');
     app.get('/complete',complete.render);
+    app.post('/complete',complete.renderPost);
 }
