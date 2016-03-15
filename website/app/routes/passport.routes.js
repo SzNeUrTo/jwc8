75741b6b6b6eaf66e13d1c47eb346d955266a62d
komcal <kom.sujautra@gmail.com>
2016-03-15 14:51:03 +0700

    change to hbs

--- website/app/routes/passport.routes.js
+++ website/app/routes/passport.routes.js
@@ -32,13 +32,13 @@ module.exports = function(app, passport){
 
 	app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
 
-	app.get('/auth/facebook/callback', 
+	app.get('/auth/facebook/callback',
 	  passport.authenticate('facebook', { successRedirect: '/profile',
 	                                      failureRedirect: '/testauth' }));
 
 	app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
-	app.get('/auth/google/callback', 
+	app.get('/auth/google/callback',
 	  passport.authenticate('google', { successRedirect: '/profile',
 	                                      failureRedirect: '/testauth' }));
 
