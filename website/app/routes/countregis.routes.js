70b843fb8ab8c82b027028360628c264920315c0
Supanut <supanut.overload@gmail.com>
2016-03-17 08:00:11 +0700

    Dynamic OAuth URL

--- website/app/routes/countregis.routes.js
+++ website/app/routes/countregis.routes.js
@@ -1,7 +1,7 @@
 var User = require('../models/db.model.js');
 module.exports = function(app) {
 	app.get('/count/:major', function(req, res) {
-		var major = req.params.major
+		var major = req.params.major || '';
 		User.find({'jwcinfo.major': major}, function(err, users) {
 			res.send(users.length + '');
 		});
