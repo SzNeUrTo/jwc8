adcec8c8540763205a0eef66cfabddba6695532f
komcal <kom.sujautra@gmail.com>
2016-04-19 13:45:50 +0700

    fixbug

--- website/app/routes/announce.routes.js
+++ website/app/routes/announce.routes.js
@@ -70,7 +70,7 @@ module.exports = function(app, passport) {
       }
     });
   });
-  app.get('/confirm',[isLoggedIn] ,function(req, res, next){
+  app.get('/confirm',[isLoggedIn, isPass] ,function(req, res, next){
     res.render('confirm');
   })
 
@@ -112,7 +112,7 @@ module.exports = function(app, passport) {
       User.findOne({'auth.facebook.id':data}, function(err, user){
         if(err) res.send(err)
         else{
-          user.jwcinfo.specialquestion.answers[1].point = 0;
+          user.jwcinfo.specialquestion.answers[1].point = 1;
           user.save(function(err){
             if(err) throw err;
             console.log('update slip complete');
